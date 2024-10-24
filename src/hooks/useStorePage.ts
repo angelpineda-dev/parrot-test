cc09db29a8be61c90021ce80556d73f7423eeb02
angelpineda-dev <angeljpa95@gmail.com>
2024-10-24 11:47:36 -0600

    path alias

--- src/hooks/useStorePage.ts
+++ src/hooks/useStorePage.ts
@@ -1,13 +1,21 @@
+/* React */
 import { useEffect } from "react";
-import { useAxios } from "../context/axios/AxiosProvider";
-import { useUserStore } from "../store/useUserStore";
-import { IMeResponse } from "../interfaces";
+/* Libraries */
+import { useAxios } from "@/context/axios/AxiosProvider";
+/* store */
+import { useUserStore } from "@/store/useUserStore";
+/* interfaces */
+import { IMeResponse } from "@/interfaces";
 
 export default function useStorePage() {
 	const { axiosInstance } = useAxios();
 	const setStore = useUserStore((state) => state.setStore);
 	const stores = useUserStore((state) => state.stores);
 
+	/**
+	 * getMet
+	 * fetch user data and assign it to zustand store
+	 */
 	async function getMe() {
 		try {
 			const response: IMeResponse = await axiosInstance.get("/v1/users/me");
