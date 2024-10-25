eb08fd8ad95cbc749c4e6d76a92e66b3c24bc47a
angelpineda-dev <angeljpa95@gmail.com>
2024-10-25 09:07:57 -0600

    product item refactor

--- src/pages/StoreDetails/components/ProductItem/ProductItem.tsx
+++ src/pages/StoreDetails/components/ProductItem/ProductItem.tsx
@@ -1,11 +1,13 @@
-import { useState, useEffect, useRef } from "react";
-import { List, ListItemButton, ListItemText, Switch } from "@mui/material";
-import { Product } from "@/interfaces/IProductResponse";
-import { useAxios } from "@/context/axios/AxiosProvider";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router";
+import { toast } from "react-toastify";
+import { List, ListItemButton, ListItemText, Switch } from "@mui/material";
+import { useAxios } from "@/context/axios/AxiosProvider";
+import { Product } from "@/interfaces/IProductResponse";
 import { IProductUpdateResponse } from "@/interfaces/IProductUpdateResponse";
 import useStorePage from "@/hooks/useStorePage";
-import { toast } from "react-toastify";
+
+import "./styles/productItem.css";
 
 function getSwitchState(state: string) {
 	if (state == "AVAILABLE") {
@@ -15,46 +17,46 @@ function getSwitchState(state: string) {
 	}
 }
 
-const ProductItem = ({ name, uuid, availability }: Product) => {
+const ProductItem = ({ name, uuid, availability, imageUrl }: Product) => {
 	const [switchState, setSwitchState] = useState(getSwitchState(availability));
 	const { getProducts } = useStorePage();
 	const { axiosInstance } = useAxios();
 	const { id: storeID } = useParams();
-	const switchRef = useRef(false);
+	const refSwitch = useRef(false);
 
-	const handleSwitch = () => {
-		setSwitchState(!switchState);
-	};
+	async function updateProduct(uuid: string) {
+		try {
+			const response: IProductUpdateResponse = await axiosInstance.put(
+				`/v1/products/${uuid}/availability`,
+				{
+					availability: !switchState ? "AVAILABLE" : "UNAVAILABLE",
+				}
+			);
 
-	async function updateProduct(uuid: string, value: boolean) {
-		const response: IProductUpdateResponse = await axiosInstance.put(
-			`/v1/products/${uuid}/availability`,
-			{
-				availability: value ? "AVAILABLE" : "UNAVAILABLE",
+			if (response.status == "ok") {
+				toast.success("Producto actualizado correctamente.");
+				getProducts(storeID);
 			}
-		);
-
-		if (response.status == "ok" && storeID) {
-			toast.success("Producto actualizado correctamente.");
-			getProducts(storeID);
+		} catch (error) {
+			toast.error(`Error al actualizar el producto: ${name}`);
 		}
 	}
 
 	useEffect(() => {
-		/* avois first render petition */
-		if (!switchRef.current) {
-			switchRef.current = true;
+		if (!refSwitch.current) {
+			refSwitch.current = true;
 			return;
 		}
 
-		updateProduct(uuid, switchState);
-	}, [switchState]);
+		setSwitchState(getSwitchState(availability));
+	}, [availability]);
 
 	return (
 		<List component="div" disablePadding>
 			<ListItemButton sx={{ pl: 4 }}>
+				<img src={imageUrl} alt={name} className="product-img" />
 				<ListItemText primary={name} />
-				<Switch checked={switchState} onChange={handleSwitch} />
+				<Switch checked={switchState} onChange={() => updateProduct(uuid)} />
 			</ListItemButton>
 		</List>
 	);
